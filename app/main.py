0737a936c87b9ab0a0c6126e47b0090618523881
Luan Moraes Alencar <luan.moraees21@gmail.com>
2025-09-15 12:14:51 -0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,12 @@
-# write your code here
+def copy_file(command: str) -> None:
+    parts = command.split()
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+    source = parts[1]
+    target = parts[2]
+
+    try:
+        with open(source) as source_file, open(target, "w") as target_file:
+            target_file.write(source_file.read())
+    except FileNotFoundError:
+        ...
